9b66e813b2622f9fce892b1e3f1b1afc08fc63f3
aayaan14 <hasnainaayaan14@gmail.com>
2025-06-29 16:58:07 +0530

    testing cors-2

--- tickashi/backend/app/main.py
+++ tickashi/backend/app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
-
 from app.routes import todo_routes
 from app.database import Base, engine
 
@@ -14,7 +13,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # <-- TEMPORARY for testing; lock it down later
+    allow_origins=["https://tickashi.vercel.app"],  # or ["*"] just for debugging
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
