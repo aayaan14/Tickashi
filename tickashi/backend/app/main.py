385613ce899fa2e0d4c1e072a768bf16abdf2dfc
aayaan14 <hasnainaayaan14@gmail.com>
2025-06-29 19:46:15 +0530

    testing cors-4

--- tickashi/backend/app/main.py
+++ tickashi/backend/app/main.py
@@ -1,26 +1,25 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.routes import todo_routes  
-from app.database import engine, Base
-from app.models.todo_model import Todo
+from app.routes import todo_routes
+from app.database import Base, engine
 
-# Create the tables
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# Allow frontend origins (Vercel, local dev)
 origins = [
     "https://tickashi.vercel.app",
-    "http://localhost:5173"
+    "https://tickashi.vercel.app/",
+    "http://localhost:5173",
+    "http://localhost:5173/"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=origins,         
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"],          
+    allow_headers=["*"],        
 )
 
 app.include_router(todo_routes.router)
