05f4916e97b59cbff0e4dc6efe50b5370409d3c2
aayaan14 <hasnainaayaan14@gmail.com>
2025-06-29 16:48:54 +0530

    added more cors origins and lifespan

--- tickashi/backend/app/main.py
+++ tickashi/backend/app/main.py
@@ -1,24 +1,32 @@
 from fastapi import FastAPI
-from app.routes import todo_routes
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import Depends
-from sqlalchemy.orm import Session
-from app.database import SessionLocal
+from contextlib import asynccontextmanager
+from app.routes import todo_routes
 from app.database import Base, engine
-from app.models.todo_model import Todo
 
-# Create tables (only during startup)
-Base.metadata.create_all(bind=engine)
+# Lifespan event handler
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup logic
+    Base.metadata.create_all(bind=engine)
+    yield
+    # (Optional) Shutdown logic
 
-app = FastAPI(debug=True)
+app = FastAPI(lifespan=lifespan)
+
+# CORS setup
+origins = [
+    "https://tickashi.vercel.app",
+    "http://localhost:5173"
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  #  frontend's origin
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+# Register routes
 app.include_router(todo_routes.router)
-
