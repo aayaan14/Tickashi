3b9fa0b07586ab490a3bd8c63a804bf989c327ab
aayaan14 <hasnainaayaan14@gmail.com>
2025-07-01 13:10:54 +0530

    moved to supabase postgresql

--- tickashi/backend/app/schemas/todo_schema.py
+++ tickashi/backend/app/schemas/todo_schema.py
@@ -2,14 +2,14 @@ from pydantic import BaseModel
 
 # Schema for creating a new todo
 class TodoCreate(BaseModel):
-    task: str
-    is_done: bool = False
+    task    : str
+    is_done : bool = False
 
 # Schema for responding with todo data (e.g., after reading from DB)
 class TodoResponse(BaseModel):
-    id: int
-    task: str
-    is_done: bool
+    id      : int
+    task    : str
+    is_done : bool
 
     class Config:
         orm_mode = True  # Enables reading data from SQLAlchemy model instances
